c7e715d3d7afad0c82c7c16da78dc9a8f17803e4
Michael Hayes <michaelhayes290@gmail.com>
2018-02-11 16:48:51 -0500

    Get ECB-Hex Encode/Decode Working

--- TEA/src/edu/cedarville/cs/crypto/TinyE.java
+++ TEA/src/edu/cedarville/cs/crypto/TinyE.java
@@ -5,11 +5,87 @@ public class TinyE {
 	public static enum Mode { ECB, CBC, CTR };
 	
 	public Integer[] encrypt(Integer[] plaintext, Integer[] key, Mode mode, Integer[] iv) {
-		return null;
+            
+            Integer output[] = new Integer[plaintext.length];
+            
+            if(mode.equals(TinyE.Mode.ECB)){
+                //encrypt each block individually
+                for(int i=0; i<plaintext.length/2; i+=2){
+                    Integer temp[] = new Integer[2];
+                    temp = encryptBlock(plaintext[i], plaintext[i+1], key);
+                    output[i] = temp[0];
+                    output[i+1] = temp[1];
+                }
+                return output;
+            }
+            else if(mode.equals(TinyE.Mode.CBC)){
+                
+            }
+            else if(mode.equals(TinyE.Mode.CTR)){
+                
+            }
+            
+            return null;
 	}
 
 	public Integer[] decrypt(Integer[] ciphertext, Integer[] key, Mode mode, Integer[] iv) {
-		return null;
+            
+            Integer output[] = new Integer[ciphertext.length];
+            
+            if(mode.equals(TinyE.Mode.ECB)){
+                //encrypt each block individually
+                for(int i=0; i<ciphertext.length/2; i+=2){
+                    Integer temp[] = new Integer[2];
+                    temp = decryptBlock(ciphertext[i], ciphertext[i+1], key);
+                    output[i] = temp[0];
+                    output[i+1] = temp[1];
+                }
+                return output;
+            }
+            else if(mode.equals(TinyE.Mode.CBC)){
+                
+            }
+            else if(mode.equals(TinyE.Mode.CTR)){
+                
+            }
+            
+            return null;
 	}
+        
+        private static Integer[] encryptBlock(Integer L, Integer R, Integer[] K){
+            
+            Integer delta = Integer.parseUnsignedInt("9e3779b9", 16);
+            Integer sum = 0;
+            
+            for(int i=0; i<32; i++){
+                sum += delta;
+                L += ((R<<4)+K[0])^(R+sum)^((R>>5)+K[1]);
+                R += ((L<<4)+K[2])^(L+sum)^((L>>5)+K[3]);
+            }
+            
+            Integer output[] = new Integer[2];
+            output[0] = L;
+            output[1] = R;
+            
+            return output;
+        }
+        
+        private static Integer[] decryptBlock(Integer L, Integer R, Integer[] K){
+            
+            Integer delta = Integer.parseUnsignedInt("9e3779b9", 16);
+            Integer sum = delta << 5;
+            
+            for(int i=0; i<32; i++){
+                R -= ((L<<4)+K[2])^(L+sum)^((L>>5)+K[3]);
+                L -= ((R<<4)+K[0])^(R+sum)^((R>>5)+K[1]);
+                sum -= delta;
+            }
+            
+            Integer output[] = new Integer[2];
+            output[0] = L;
+            output[1] = R;
+            
+            return output;
+        }
 	
 }
--- TEA/src/edu/cedarville/cs/crypto/Tools.java
+++ TEA/src/edu/cedarville/cs/crypto/Tools.java
@@ -3,19 +3,50 @@ package edu.cedarville.cs.crypto;
 public class Tools {
 	
 	public static Integer[] convertFromBytesToInts(byte[] bs) {
-		return null;
+                
+            Integer output[] = new Integer[bs.length];
+            
+            for(int i=0; i < bs.length; i++){
+                output[i] = (int) bs[i];
+            }
+            
+            return output;
 	}
 	
 	public static Integer[] convertFromHexStringToInts(String s) {
-		return null;
+		
+            //unsigned integers are represented by 8 hex characters
+            Integer output[] = new Integer[s.length()/8];
+            
+            int j=0;
+            for(int i=0; i < s.length()/8; i++){
+                output[i] = Integer.parseUnsignedInt(s.substring(j, j+8), 16);
+                j+=8;
+            }
+            
+            return output;
 	}
 	
 	public static byte[] convertFromIntsToBytes(Integer[] ints) {
-		return null;
+		
+            byte output[] = new byte[ints.length];
+            
+            for(int i=0; i < ints.length; i++){
+                output[i] = ints[i].byteValue();
+            }
+
+            return output;
 	}
 	
 	public static String convertFromIntsToHexString(Integer[] ints) {
-		return null;
+            
+            String s = "";
+            
+            for(int i=0; i < ints.length; i++){
+                s = s.concat(Integer.toHexString(ints[i]));
+            }
+            
+            return s;
 	}
 	
 }
